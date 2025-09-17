f1e5a82eb6ef3dd15a70858656b57bf9760e6bf3
Amaia <amaiavzqz@gmail.com>
2025-09-17 12:26:37 +0200

    adding hello world react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Hello World desde React</h1>
+      <p>App sencilla lista para Docker y AKS</p>
+    </div>
+  );
+}
+
+export default App;
